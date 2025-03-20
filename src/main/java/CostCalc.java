6f328cf3fc783a4f193cc5f4333298f0e4e74a00
Heta <hetahartzell@protonmail.com>
2025-03-20 10:30:53 +0200

    updates

--- src/main/java/CostCalc.java
+++ src/main/java/CostCalc.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class CostCalc {
@@ -10,15 +12,48 @@ public class CostCalc {
     }
 
     public static void main(String[] args) {
-        double consumption = (5/100.0);
+        System.out.println("Select a language: ");
+        System.out.println("1. Farsi");
+        System.out.println("2. Finnish");
+        System.out.println("3. Japanese");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the distance in km: ");
+        int choice = scanner.nextInt();
+
+        Locale locale = switch (choice) {
+            case 1 -> {
+                System.out.println("You selected Farsi.");
+                yield new Locale("fa", "IR");
+            }
+            case 2 -> {
+                System.out.println("You selected Finnish.");
+                yield new Locale("fi", "FI");
+            }
+            case 3 -> {
+                System.out.println("You selected Japanese.");
+                yield new Locale("ja", "JP");
+            }
+            default -> {
+                System.out.println("Invalid language.");
+                yield new Locale("en", "US");
+            }
+        };
+
+        ResourceBundle rb;
+        try {
+            rb = ResourceBundle.getBundle("messages", locale);
+        } catch (Exception e) {
+            System.out.println("Invalid language.");
+            rb = ResourceBundle.getBundle("messages", new Locale("en", "US"));
+        }
+
+        double consumption = (5/100.0);
+        System.out.println(rb.getString("kilo"));
         double distance = scanner.nextDouble();
-        System.out.println("Enter the price per liter: ");
+        System.out.println(rb.getString("price"));
         double price = scanner.nextDouble();
 
-        System.out.println("The cost of the trip is: " + calculateCost(consumption, distance, price));
-        System.out.println("The fuel needed for the trip is: " + calculateFuelNeeded(consumption, distance));
+        System.out.println(rb.getString("total") + calculateCost(consumption, distance, price) + "€");
+        System.out.println(rb.getString("fuel") + calculateFuelNeeded(consumption, distance) + "L");
         scanner.close();
     }
 }
